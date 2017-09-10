e0f12c4cea3f14ccf3be348aaceaf0cfbc0fc6c9
Vladimir Aguilar <vladimir.aguilar@ucr.ac.cr>
2017-09-09 22:43:40 -0600

    Se agregan formas de parsear el XML

---
+++ src/main/java/parsers/BeanProperty.java
@@ -0,0 +1,30 @@
+package parsers;
+
+/**
+ * Creation Date: 9/9/2017
+ */
+public enum BeanProperty {
+    ID,
+    CLASS,
+    SCOPE,
+    INIT,
+    DESTROY,
+    ERROR;
+
+    public static BeanProperty getProperty(String propertyName){
+        propertyName = propertyName.toLowerCase();
+        BeanProperty property = ERROR;
+        if(propertyName.equals("id")){
+            property = ID;
+        }else if(propertyName.equals("class")){
+            property = CLASS;
+        }else if(propertyName.equals("init-method")){
+            property = INIT;
+        }else if(propertyName.equals("destroy-method")){
+            property = DESTROY;
+        }else if(propertyName.equals("scope")){
+            property = SCOPE;
+        }
+        return property;
+    }
+}
