49fd0a5bc6ece0dd88f36d49cd5f42239107541d
racuna07 <rodrigo.acuna.f@gmail.com>
2017-09-09 23:25:16 -0900

    asd

--- src/main/java/context/AnnotationApplicationContext.java
+++ src/main/java/context/AnnotationApplicationContext.java
@@ -7,4 +7,36 @@ import parsers.AnnotationParser;
  */
 public class AnnotationApplicationContext extends ApplicationContext {
     private AnnotationParser annotationParser;
+
+    public Object getBean(String beanId) {
+        return null;
+    }
+
+    public <T> T getBean(Class<T> classType, String beanId) {
+        return null;
+    }
+
+    public boolean containsBean(String beanId) {
+        return false;
+    }
+
+    public boolean isSingleton(String beanId) {
+        return false;
+    }
+
+    public boolean isPrototype(String beanId) {
+        return false;
+    }
+
+    public void registerBeans() {
+
+    }
+
+    public void injectDependencies() {
+
+    }
+
+    public void close() {
+
+    }
 }
--- src/main/java/context/XmlApplicationContext.java
+++ src/main/java/context/XmlApplicationContext.java
@@ -5,6 +5,38 @@ import parsers.XmlParser;
 /**
  * Created by Rodrigo on 9/9/2017.
  */
-public class XmlApplicationContext extends ApplicationContext {
+public  class XmlApplicationContext extends ApplicationContext {
     private XmlParser xmlParser;
+
+    public Object getBean(String beanId) {
+        return null;
+    }
+
+    public <T> T getBean(Class<T> classType, String beanId) {
+        return null;
+    }
+
+    public boolean containsBean(String beanId) {
+        return false;
+    }
+
+    public boolean isSingleton(String beanId) {
+        return false;
+    }
+
+    public boolean isPrototype(String beanId) {
+        return false;
+    }
+
+    public void registerBeans() {
+
+    }
+
+    public void injectDependencies() {
+
+    }
+
+    public void close() {
+
+    }
 }
