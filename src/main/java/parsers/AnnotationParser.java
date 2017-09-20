427f06084c749dd1beffee9d12cf1681b5bee931
racuna07 <rodrigo.acuna.f@gmail.com>
2017-09-19 15:45:27 -0900

    Cambio a scannear clases del folder classes.

--- src/main/java/parsers/AnnotationParser.java
+++ src/main/java/parsers/AnnotationParser.java
@@ -12,49 +12,49 @@ import java.net.URL;
  * Creation Date: 9/9/2017
  */
 public class AnnotationParser implements Parser {
+    String basePackagePath;
 
     public AnnotationParser(String packageLocation){
         //linea
-        // String classesRootDirectory = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-        //String basePackagePath = classesRootDirectory+packageLocation.replaceAll(".","/");
-        File basePackage = new File("src/main/java/"+packageLocation);
-        System.out.println(basePackage.getAbsolutePath());
+        String classesRootDirectory = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+        File basePackage = new File(classesRootDirectory+packageLocation.replaceAll(".","/"));
+        basePackagePath = basePackage.getAbsolutePath();
+        System.out.println(basePackagePath);
 
         if(!basePackage.exists() || !basePackage.isDirectory())
             System.out.println("Incorrect Base Package");
         else {
-            try {
-                scanPackage(basePackage);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            scanPackage(basePackage);
         }
     }
 
-    public void scanPackage(File currentPackage) throws ClassNotFoundException, IOException {
+    public void scanPackage(File currentPackage){
         File[] packageContents = currentPackage.listFiles();
         for(File file: packageContents){
             if(file.isDirectory())
                 scanPackage(file);
             else{
-                String className = file.getName().replace(".java","");
-                String claseFinal = currentPackage.getPath()+"\\"+className;
-                System.out.println(claseFinal);
-
-                Class clase = Class.forName(claseFinal.replace('\\','.').replace("src.main.java.",""));
-                Annotation[] annotations = clase.getAnnotations();
-                for (Annotation annotation: annotations){
-                    System.out.println(annotation);
-                }
+                String className = file.getName().replace(".class","");
+                String currentPath = currentPackage.getPath().substring(basePackagePath.length()+1);
+                String classPath = currentPath.replace("\\",".") + "." + className;
+                scanClass(classPath);
 
             }
         }
 
     }
 
-
+    private void scanClass(String classPath) {
+        try {
+            Class currentClass = Class.forName(classPath);
+            Annotation[] annotations = currentClass.getAnnotations();
+            for (Annotation annotation:annotations) {
+                System.out.println(annotation.annotationType());
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
---
+++ src/main/java/tests/test1/Test2.java
@@ -0,0 +1,7 @@
+package tests.test1;
+
+/**
+ * Created by Rodrigo on 9/10/2017.
+ */
+public class Test2 {
+}
